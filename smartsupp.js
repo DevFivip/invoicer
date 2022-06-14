3edd8d36e4a9513548ee4b22387a99337280bd16
Adolfo Gabazutt <adolfo.gbztt@gmail.com>
2022-06-13 20:14:28 -0500

    feat: hot fix

---
+++ smartsupp.js
@@ -0,0 +1,18 @@
+var _smartsupp = _smartsupp || {};
+_smartsupp.key = "a8f8c42064acb68d567d1a0e5c25a5bf76d5b422";
+window.smartsupp ||
+  (function (d) {
+    var s,
+      c,
+      o = (smartsupp = function () {
+        o._.push(arguments);
+      });
+    o._ = [];
+    s = d.getElementsByTagName("script")[0];
+    c = d.createElement("script");
+    c.type = "text/javascript";
+    c.charset = "utf-8";
+    c.async = true;
+    c.src = "https://www.smartsuppchat.com/loader.js?";
+    s.parentNode.insertBefore(c, s);
+  })(document);
